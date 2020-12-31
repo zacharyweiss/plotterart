76726ea786251e0f93ad29aaa21db7ebd5550074
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-12-30 22:56:14 -0500

    Duffing

---
+++ forcedduffing.py
@@ -0,0 +1,27 @@
+from diffeqpy import de
+import matplotlib.pyplot as plt
+import numpy as np
+import numba
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def f(u, p, t):
+    # assumes omega_0 = 1
+    x, y = u
+    alpha, beta, delta, gamma, omega, phi = p
+    return [y, x-beta*pow(x, 3)-delta*y+gamma*np.cos(omega*t + phi)]
+
+
+u0 = [1.0, 0.0]
+tspan = (0., 10.)
+p = [1., 5., 0.02, 8., 0.5, 0.]
+numba_f = numba.jit(f)
+prob = de.ODEProblem(numba_f, u0, tspan, p)
+sol = de.solve(prob, saveat=0.01)
+
+ut = np.transpose(sol.u)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(ut[0,:],ut[1,:])
+plt.show()
