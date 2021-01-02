21fa8e5b4ca931c24c2628645b18fbda3e784a67
Zachary Weiss <zachary.weiss.acct@gmail.com>
2021-01-02 18:58:03 -0500

    Git ignore

--- feynmanellipse.py
+++ feynmanellipse.py
@@ -3,5 +3,14 @@ import matplotlib.pyplot as plt
 import math
 
 nlines = 50
-radius = 5
-point = 
+radius = 1
+point = [0.9, 0]
+
+if sum(np.square(point)) >= radius:
+    raise ValueError('Point is outside of or on circle')
+
+intersections = []
+for i in range(0, nlines-1):
+    # can likely do this smarter array-wise, but just implementing fast here
+    theta = i/(nlines*2*math.pi)
+    intersections = np.column_stack(intersections, )
